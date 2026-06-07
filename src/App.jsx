563a813b55e3f9d2e9af07777afd269808e39166
mrzyron1227 <crasheron@gmail.com>
2026-06-07 19:57:26 +0700

    Fix: Kembalikan ke prefix VITE via vercel.json

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import { useState } from 'react';
 import { createClient } from '@supabase/supabase-js';
 
 // Vercel otomatis membaca variabel ini karena prefix VITE_ tadi
-const supabaseUrl = import.meta.env.SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 export default function App() {
